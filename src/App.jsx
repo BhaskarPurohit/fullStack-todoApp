9a606731e2a4b205960dac56440b58a74a7b710e
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-12-09 15:08:51 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
 
+
+function App() {
+  const [todo, setTodo] = useState(
+     {
+      title:"go gym",
+      description:"lift heavy",
+      id:1
+    })
+
+    setInterval(()=>{
+      setTodo({
+        title:"dinner",
+        description:"dinner banao eggs",
+        id:2
+      })
+    },2000)
+
+ 
   return (
     <>
       <div className="app">
         <h1>Vite app</h1>
+        <p>{todo.id}</p>
+        <p>{todo.title}</p>
+        <p>{todo.description}</p>
       </div>
     </>
   )
